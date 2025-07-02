bf04e518162b17a3d02f8a5a4d27c41108bd83ee
VarshithaGovind <167661585+VarshithaGovind@users.noreply.github.com>
2025-07-03 01:00:08 +0530

    Delete backend_web-main directory

--- backend_web-main/models/AccessLog.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const accessLogSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  action: String,
-  timestamp: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('AccessLog', accessLogSchema);
--- backend_web-main/routes/Booking.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { bookMajor } = require('../controller/bookingController');
-
-// Removed `auth` middleware
-router.post('/booking', bookMajor);
-
-module.exports = router;
